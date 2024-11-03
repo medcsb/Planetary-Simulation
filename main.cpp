2b52ff9d6dc92369d7274766ddfc6439c05c23a2
medcsb <medchsb@gmail.com>
2024-11-03 03:06:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,3 @@
-// ----------------------------------------------------------------------------
-// main.cpp
-//
-//  Created on: 24 Jul 2020
-//      Author: Kiwon Um
-//        Mail: kiwon.um@telecom-paris.fr
-//
-// Description: IGR201 Practical; OpenGL and Shaders (DO NOT distribute!)
-//
-// Copyright 2020-2024 Kiwon Um
-//
-// The copyright to the computer program(s) herein is the property of Kiwon Um,
-// Telecom Paris, France. The program(s) may be used and/or copied only with
-// the written permission of Kiwon Um or in accordance with the terms and
-// conditions stipulated in the agreement/contract under which the program(s)
-// have been supplied.
-// ----------------------------------------------------------------------------
-
 #include "mesh.h"
 #include "camera.h"
 
